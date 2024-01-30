6b38adf054bea686ef9e54184849192898684254
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-01-30 07:15:11 +0000

    More boilerplate

---
+++ service.py
@@ -0,0 +1,9 @@
+from common.service import Service
+
+class BeatmapsService(Service):
+    
+    def __init__(self) -> None:
+        super().__init__("beatmaps_svc", True)
+
+    def run(self):
+        pass
