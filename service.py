fde2df4bc97783308b044a8d3abe44ba98178fc2
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-02-13 00:33:05 +0100

    Store beatmap packs

--- service.py
+++ service.py
@@ -2,4 +2,4 @@ from common.service import TaskedService
 from beatmaps.tasks import *
 
 def get_service() -> TaskedService:
-    return TaskedService("beatmaps_svc", [UpdateBanchoMaps()])
+    return TaskedService("beatmaps_svc", [UpdateBanchoMaps(), UpdateBeatmapPacks()])
--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-from common.database.objects import DBBeatmapset
+from common.database.objects import DBBeatmapset, DBBeatmapPack
 from common.service import RepeatedTask
 from common.app import database, ossapi
 from common.logging import get_logger
@@ -6,6 +6,8 @@ from common.logging import get_logger
 from ossapi import *
 
 import common.repos.beatmaps as beatmaps
+
+import time
 
 logger = get_logger("beatmaps_svc")
 
@@ -41,3 +43,43 @@ class UpdateBanchoMaps(RepeatedTask):
                     break
             logger.info(f"Added {added} beatmapsets.")
         return True
+    
+class UpdateBeatmapPacks(RepeatedTask):
+    
+    def __init__(self) -> None:
+        super().__init__("update_beatmap_packs", 2400)
+        
+    def run(self):
+        added = 0
+        with database.session as session:
+            cursor = None
+            while True:
+                packs = ossapi.beatmap_packs(cursor_string=cursor)
+                cursor = packs.cursor_string
+                if not packs.beatmap_packs:
+                    break
+                for pack_compact in packs.beatmap_packs:
+                    if session.get(DBBeatmapPack, pack_compact.tag):
+                        cursor = None
+                        break
+                    pack = ossapi.beatmap_pack(pack_compact.tag)
+                    sets_id = list()
+                    for beatmapset in pack.beatmapsets:
+                        if beatmaps.get_beatmapset(beatmapset.id, force_fetch=True):
+                            sets_id.append(beatmapset.id)
+                    added += 1
+                    session.add(DBBeatmapPack(
+                        author=pack.author,
+                        date=pack.date,
+                        name=pack.name,
+                        link=pack.url,
+                        tag=pack.tag,
+                        no_diff_reduction=pack.no_diff_reduction,
+                        beatmapsets=sets_id
+                    ))
+                    logger.debug(f"Added beatmap pack {pack.tag}.")
+                    time.sleep(0.2)
+                if not cursor:
+                    break
+            session.commit()
+            logger.info(f"Added {added} beatmap packs.")
