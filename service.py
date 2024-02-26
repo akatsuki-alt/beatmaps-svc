155da4a6b0b96903e40b5f09304c91d513b770f8
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-02-26 20:51:48 +0100

    Add akatsuki beatmap tracking

--- service.py
+++ service.py
@@ -2,4 +2,4 @@ from common.service import TaskedService
 from beatmaps.tasks import *
 
 def get_service() -> TaskedService:
-    return TaskedService("beatmaps_svc", [UpdateBanchoMaps(), UpdateBeatmapPacks(), UpdateQualifiedMaps()])
+    return TaskedService("beatmaps_svc", [UpdateAkatsukiMaps(), UpdateBanchoMaps(), UpdateBeatmapPacks(), UpdateQualifiedMaps()])
--- tasks.py
+++ tasks.py
@@ -1,4 +1,6 @@
-from common.database.objects import DBBeatmapset, DBBeatmapPack, DBBeatmap
+from typing import List, Tuple
+from common.database.objects import DBBeatmapset, DBBeatmapPack, DBBeatmap, DBTask
+from sqlalchemy.orm.attributes import flag_modified
 
 from common.service import RepeatedTask
 from common.app import database, ossapi
@@ -9,8 +11,11 @@ from sqlalchemy import Integer
 
 from ossapi import *
 
+import beatmaps.selfbot as selfbot
 import common.repos.beatmaps as beatmaps
 import time
+
+from beatmaps.selfbot import Embed
 
 logger = get_logger("beatmaps_svc")
 
@@ -58,16 +63,71 @@ class UpdateQualifiedMaps(RepeatedTask):
                 if map.set_id in sets:
                     continue
                 # TODO: check if beatmaps got deleted
-                beatmaps.get_beatmapset(map.set_id, force_fetch=True) # TODO: pass session
+                beatmaps.get_beatmapset(map.set_id, force_fetch=True, session=session)
                 sets.append(map.set_id)
             for map in session.query(DBBeatmap).filter(DBBeatmap.status['akatsuki'].astext.cast(Integer) == 3, (datetime.now() - DBBeatmap.last_db_update) > timedelta(days=1)):
                 if map.set_id in sets:
                     continue
                 # Same
-                beatmaps.get_beatmapset(map.set_id, force_fetch=True) # TODO: pass session
+                beatmaps.get_beatmapset(map.set_id, force_fetch=True, session=session)
                 sets.append(map.set_id)
             session.commit()
             return True
+
+class UpdateAkatsukiMaps(RepeatedTask):
+    
+    def __init__(self):
+        super().__init__("update_akatsuki_maps", 2400)
+
+    def can_run(self) -> bool:
+        
+        return super().can_run()
+
+    def parse_akatsuki_embed(self, embed: Embed) -> Tuple[int, str]:
+        if 'url' in embed and embed['url']: # new embed format
+            beatmap_id = int(embed['url'].split("/")[-1])
+            nominator = embed['author']['name'].split()[0]
+            return beatmap_id, nominator
+        else:
+            if embed['fields'][3]['name'] == "Gamemode": # very old embed format
+                beatmap_id = int(embed['fields'][4]['value'].split("/")[-1][:-1])
+            else:
+                beatmap_id = int(embed['fields'][3]['value'].split("/")[-1][:-1])
+            nominator = embed['fields'][2]['value'].split("]")[0][1:]
+            return beatmap_id, nominator
+
+    def run(self):
+        logger.info("Fetching akatsuki beatmaps...")
+        found: List[selfbot.Message] = list()
+        with database.managed_session() as session:
+            full_run = session.get(DBTask, self.task_name) is None
+            for x in range(0, 10000000 if full_run else 250 ,25):
+                messages = selfbot.search_channel(offset=x)
+                if not messages:
+                    logger.info('messages done.')
+                    break
+                found.extend(messages)
+            found.reverse()
+            maps = list()
+            for message in found:
+                message = message[0]
+                if not message['embeds']:
+                    continue
+                embed = message['embeds'][0]
+                maps.append(self.parse_akatsuki_embed(embed))
+            logger.info(f"Found {len(maps)} Akatsuki updates")
+
+            for beatmap_id, nominator in maps:
+                if (beatmap := beatmaps.get_beatmap(beatmap_id, force_fetch=True, session=session)):
+                    beatmap.status['akatsuki'] = -1
+                    beatmap.beatmapset.nominators['akatsuki'] = [nominator]
+                    flag_modified(beatmap, 'status')
+                    flag_modified(beatmap.beatmapset, 'nominators')
+                else:
+                    pass # Deleted map most likely
+                session.commit()
+                session.flush()
+        return True
 
 class UpdateBeatmapPacks(RepeatedTask):
     
